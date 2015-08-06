a425dec7b577aae6df4c029f677567c6d0515833
ZeeDrake <ZeeDrake3000@gmail.com>
2015-08-06 18:47:00 -0500

    Set up new, create &  edit pages, added delete link

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,42 @@ class ArticlesController < ApplicationController
 
   def show
   	@article = Article.find(params[:id])
+    @category = Category.all
+    @author = Author.all
   end
 
   def edit
+      @article = Article.find(params[:id]) 
   end
 
   def new
+    @article = Article.new
+    @category = Category.all
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to articles_path, :notice => "Article has been saved"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(params[:article])
+      redirect_to article_path, :notice => "Your article has been updated"
+    else
+      render "edit"
+    end 
+  end
+
+  def destory
+    @article = Article.find(params[:id])
+    @article.destory
+    redirect_to articles_path, :notice => "Your article has been deleted"
+ 
+    
   end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+	@mom = Category.find(1)
+	@family = Category.find(2)
+	@train = Category.find(3)
+	@shop = Category.find(4)
+
   end
 end
+
+  
